ac14c999751fd7c29e639410f118ef36f4d9a599
Ari von Nordenskjöld <admin@vnord.net>
2019-12-25 18:44:14 +0100

    limit search to released films, add runtime attribute

--- src/filmtoss/core.clj
+++ src/filmtoss/core.clj
@@ -4,10 +4,14 @@
 (defn imdb-search-url [film-title]
   (clojure.string/replace (str "https://www.imdb.com/search/title/?title="
        film-title
-       "&title_type=feature&lang=en_us") " " "+"))
+       "&title_type=feature&release_date=," (today)) " " "+"))
 
 (defn fetch-url [url]
   (html/html-resource (java.net.URL. url)))
+
+(defn today []
+  (.format (java.text.SimpleDateFormat. "yyyy-MM-dd") (new java.util.Date)))
+
 
 (defn get-id-from-attr [x]
   (re-find #"\d+" (:href (:attrs x))))
@@ -49,14 +53,29 @@
 (defn get-original-title [page]
   (first (:content (first (html/select page [:div.originalTitle])))))
 
+(defn get-genres [page loc]
+  (let [g (map clojure.string/trim (filter #(not= "|" %) (filter string?
+    (flatten (map :content (:content (first (html/select page
+      [[:div.see-more.inline.canwrap (html/nth-of-type loc)]]))))))))]
+  (if (empty? g) nil
+    (if (clojure.string/starts-with? (first g) "Genre") (rest g) nil))))
+
+(defn get-runtime [page]
+  (->> (html/select page [:time]) (map :content) rest first first
+       (re-find #"\d+")))
+
 (defn get-info [title]
-  (let [id              (first (imdb-ids title))
-        page            (fetch-url (str "https://www.imdb.com/title/tt" id))
-        title           [:title (get-title page)]
-        original-title  [:original-title (get-original-title page)]
-        ppl             (get-people page)
-        desc            [:desc (clojure.string/trim (first (map html/text
-                               (html/select page [:div.summary_text]))))]
-        rating [:rating (get-rating page)]
-        year   [:year (get-year page)]]
-    (into {} [[:id id] title original-title year rating ppl desc])))
+  (let [id             (first (imdb-ids title))
+        page           (fetch-url (str "https://www.imdb.com/title/tt" id))
+        title          [:title (get-title page)]
+        original-title [:original-title (get-original-title page)]
+        ppl            (get-people page)
+        desc           [:desc (clojure.string/trim (first (map html/text
+                              (html/select page [:div.summary_text]))))]
+        rating         [:rating (get-rating page)]
+        year           [:year (get-year page)]
+        genre          [:genre (filter string? (flatten
+                                 (map #(get-genres page %) [3 4 5])))]
+        runtime        [:runtime (get-runtime page)]]
+    (into {}
+      [[:id id] title original-title year rating ppl desc genre runtime])))
